3b97007ad8de7fef8e8c9f98fb001a3baf67eed2
Varun Khachane <varunkhachane31@gmail.com>
2022-03-12 09:51:27 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class TickTock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState(
+      {
+        date:new Date()
+      }
+    );
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<TickTock/>, document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
